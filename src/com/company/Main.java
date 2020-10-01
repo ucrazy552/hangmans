88ed0f1c4ac7a823ee7b788079397fd784223aea
tomas <128593@student.drenthecollege.nl>
2020-10-01 16:24:20 +0200

    MOI EEM

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        new Poppetje();
     }
     public static  void lol() {
         System.out.println("Hello");
+
     }
 }
