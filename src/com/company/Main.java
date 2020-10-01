a62ac308135f26d5bd097bf8f2a0f610e57645b3
tomas <128593@student.drenthecollege.nl>
2020-10-01 16:05:57 +0200

    MOI EEM

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Fuck uFF");
         System.out.println("hELLO my name is Roy");
+        System.out.println("TOMAS");
     }
 }
