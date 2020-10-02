6dac3faf5a706d1f038b45387bf3fd038f5f0c5d
tomas <128593@student.drenthecollege.nl>
2020-10-02 12:06:08 +0200

    MOI EEM

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,5 +1,26 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game {
+
+    //string van woorden maken
+    public static String[] words = {"Mercedes ", "Ferrari", "RedBull", "Renault", "HAAS", "AlfaRomeo"};
+
+    public static int randString(){
+        Random rand = new Random();
+        int res = rand.nextInt(words.length);
+        return res;
+    }
+
+
+    public void input() {
+        //maken van scanner object
+        Scanner myobj = new Scanner(System.in);
+        System.out.println("Welcome to HangMan!");
+        System.out.println(randString());
+
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
         Poppetje pop = new Poppetje();
+        pop.lol();
+        lol();
+
+
+
     }
     public static  void lol() {
+        Game maker = new Game();
+        maker.input();
         System.out.println("Hello");
 
     }
